0bcad5d1471ee48185f9abf26139c243d7a34674
ttw225 <P76084106@ncku.edu.tw>
2019-09-16 21:20:31 +0800

    feat(resource/bems.py): multithread uploading data with different nodes

--- platform/resources/get_address.py
+++ platform/resources/get_address.py
@@ -1,9 +1,34 @@
+from flask import g
 from flask_restful import Resource
+from flask_httpauth import HTTPTokenAuth
+from config import app
+from utils.logging import logging
+
+auth = HTTPTokenAuth(scheme='Token')
+
+tokens = app.config['TOKEN']
+
+@auth.verify_token
+def verify_token(token):
+    if token in tokens:
+        g.current_user = tokens[token]
+        logging.info("User Login: %s" % g.current_user)
+        return True
+    return False
 
 class Get_address (Resource):
     # token check
-    def post(self, tx_hash):
-        # Generate address
+    @auth.login_required
+    def post(self, name):
+        # Check time
+        
+        # Check field
+        if name not in app.config['ADDRESS']:
+            # Generate new address
+            pass
+        address = app.config['ADDRESS'][name]
+        logging.info("[Get_address Request]\nUser:%s\nField:%s\nReturn Address:%s" % (g.current_user, name, address))
+
         return {
-            'message': "OK"
+            'address': address
         }, 200
