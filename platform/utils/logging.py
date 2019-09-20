e1214fbf61552ebdd0354c755d7f69a3738e2617
ttw225 <P76084106@ncku.edu.tw>
2019-09-20 11:40:40 +0800

    style(logging.py): change datetime format

--- platform/utils/logging.py
+++ platform/utils/logging.py
@@ -1,4 +1,4 @@
 import logging
 split = '-'*70
 logging.basicConfig(level=logging.DEBUG,
-                    format='[%(levelname)s] - %(asctime)s\n%(message)s\n'+split, datefmt='%Y%m%dT%H%M%S')
+                    format='[%(levelname)s] - %(asctime)s\n%(message)s\n'+split, datefmt='%Y-%m-%dT%H:%M:%S')
