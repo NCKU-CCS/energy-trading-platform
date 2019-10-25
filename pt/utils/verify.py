530a660b9a0aa078dfdd95f72f5ba30b44d117f6
ttw225 <P76084106@ncku.edu.tw>
2019-10-25 22:13:44 +0800

    fix(iota_tracker.py): remove plat.py and add iota_tracker.py
    iota_tracker.py is used to download IOTA's data, for default running per minutes

--- pt/utils/verify.py
+++ pt/utils/verify.py
@@ -2,6 +2,7 @@ import json
 import hashlib
 import base64
 import requests
+
 from config import APP as app
 
 
@@ -10,4 +11,4 @@ def verify_data(transaction_hash, data):
     json_data = json.dumps(data).encode('utf-8')
     hash_data = hashlib.sha256(json_data).hexdigest().encode()
     base64_data = base64.b64encode(hash_data).decode()
-    return bool(base64_data == message['message']['value'])
+    return base64_data == message['message']['value']
