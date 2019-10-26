b2dd1c4aca9974189cb10a66b797e48d792c8b1d
ttw225 <P76084106@ncku.edu.tw>
2019-10-26 17:22:43 +0800

    fix(config/__init__.py): fix bugs

--- pt/config/__init__.py
+++ pt/config/__init__.py
@@ -13,33 +13,43 @@ load_dotenv()
 
 # pylint: disable=C0103
 app = Flask(__name__)
-SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URL', 'postgresql://dev_user:dev191026@localhost:5432/dev_db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DB_URL", "postgresql://dev_user:dev191026@localhost:5432/dev_db"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 # pylint: enable=C0103
 
-SEED = os.environ.get('SEED', 'OLYBZRTBYZCJXCJR9WTSHTVXJ9DTWSREFAXIAFPPEAXJAWINDFAOPCSTGVHSJQ9DJRYOUPAEZVLZKWHTZ')
-API = iota.Iota(
-    os.environ.get('API_URI_TRACKER', 'https://nodes.thetangle.org:443'),
-    SEED,
+SEED = os.environ.get(
+    "SEED",
+    "OLYBZRTBYZCJXCJR9WTSHTVXJ9DTWSREFAXIAFPPEAXJAWINDFAOPCSTGVHSJQ9DJRYOUPAEZVLZKWHTZ",
 )
-API_TRACK = iota.Iota(os.environ.get('API_URI_TRACKER', 'https://nodes.thetangle.org:443'))
+API = iota.Iota(
+    os.environ.get("API_URI_TRACKER", "https://nodes.thetangle.org:443"), SEED
+)
+API_TRACK = iota.Iota(
+    os.environ.get("API_URI_TRACKER", "https://nodes.thetangle.org:443")
+)
 TAG_TEMPLATE = [
-    'BEMS9HOMEPAGE9INFORMATION9',
-    'BEMS9ESS9DISPLAY9',
-    'BEMS9EV9DISPLAY9',
-    'BEMS9PV9DISPLAY9',
-    'BEMS9WT9DISPLAY9'
+    "BEMS9HOMEPAGE9INFORMATION9",
+    "BEMS9ESS9DISPLAY9",
+    "BEMS9EV9DISPLAY9",
+    "BEMS9PV9DISPLAY9",
+    "BEMS9WT9DISPLAY9",
 ]
 
 # random
 RANDOM_GENERATOR = Random.new().read
 
-PEM_PATH = os.environ.get('PEM_PATH', '/home/energy-trading-platform/pt/rsa')
+PEM_PATH = os.environ.get("PEM_PATH", "/home/energy-trading-platform/pt/rsa")
 # decrypt
-PLAT_RSA_PRI_KEY = RSA.importKey(open(os.path.join(PEM_PATH, 'plat_rsa_private.pem')).read())
+PLAT_RSA_PRI_KEY = RSA.importKey(
+    open(os.path.join(PEM_PATH, "plat_rsa_private.pem")).read()
+)
 PLAT_CIPHER = Cipher_pkcs1_v1_5.new(PLAT_RSA_PRI_KEY)
 
 # signature
-AMI_RSA_PUB_KEY = RSA.importKey(open(os.path.join(PEM_PATH, 'ami_rsa_public.pem')).read())
+AMI_RSA_PUB_KEY = RSA.importKey(
+    open(os.path.join(PEM_PATH, "ami_rsa_public.pem")).read()
+)
 PLAT_SIGNER = Signature_pkcs1_v1_5.new(AMI_RSA_PUB_KEY)
