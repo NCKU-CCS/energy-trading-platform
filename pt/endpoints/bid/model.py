b1fb98daee675f87ee4277df38c75c9510eb5062
ttw225 <P76084106@ncku.edu.tw>
2019-10-28 23:40:57 +0800

    feat(endpoints/bidsubmit/resource): API: PUT BidSubmit

--- pt/endpoints/bid/model.py
+++ pt/endpoints/bid/model.py
@@ -5,31 +5,31 @@ from config import db
 
 # pylint: disable=W0611
 from utils.base_models import ETBaseMixin
-from ..user.model import User # NOQA
+from ..user.model import User  # NOQA
 
 # pylint: enable=W0611
 
 
 class Tenders(db.Model, ETBaseMixin):
-    __tablename__ = 'tenders'
+    __tablename__ = "tenders"
     uuid = db.Column(db.String(40), primary_key=True, unique=True, nullable=False)
     bid_type = db.Column(db.String(40))  # sell or buy
     start_time = db.Column(db.DateTime, unique=False, nullable=False)
     end_time = db.Column(db.DateTime, unique=False, nullable=False)
     # ForeignKey to Tenders
-    user_id = db.Column(db.String(80), db.ForeignKey('user.uuid'), nullable=False)
-    user = db.relationship('User')
+    user_id = db.Column(db.String(80), db.ForeignKey("user.uuid"), nullable=False)
+    user = db.relationship("User")
 
-    def __init__(self, tenders_data): # NOQA
+    def __init__(self, tenders_data):  # NOQA
         self.uuid = str(uuid.uuid4())
-        self.bid_type = tenders_data['bid_type']
-        self.start_time = tenders_data['start_time']
-        self.end_time = tenders_data['end_time']
-        self.user_id = tenders_data['user_id']
+        self.bid_type = tenders_data["bid_type"]
+        self.start_time = tenders_data["start_time"]
+        self.end_time = tenders_data["end_time"]
+        self.user_id = tenders_data["user_id"]
 
 
 class MatchResult(db.Model, ETBaseMixin):
-    __tablename__ = 'matchresult'
+    __tablename__ = "matchresult"
     uuid = db.Column(db.String(40), primary_key=True, unique=True, nullable=False)
     bid_type = db.Column(db.String(40))  # sell or buy
     start_time = db.Column(db.DateTime, unique=False, nullable=False)
@@ -47,50 +47,93 @@ class MatchResult(db.Model, ETBaseMixin):
     transaction_hash = db.Column(db.String(80))
     upload = db.Column(db.DateTime, unique=False, nullable=False)
     # ForeignKey to Tenders
-    tenders_id = db.Column(db.String(80), db.ForeignKey('tenders.uuid'), nullable=False)
-    tenders = db.relationship('Tenders')
+    tenders_id = db.Column(db.String(80), db.ForeignKey("tenders.uuid"), nullable=False)
+    tenders = db.relationship("Tenders")
 
     # pylint: disable=R0914,C0301
-    def __init__(self, match_data): # NOQA
+    def __init__(self, match_data):  # NOQA
         self.uuid = str(uuid.uuid4())
-        self.bid_type = match_data['bid_type']
-        self.start_time = match_data['start_time']
-        self.end_time = match_data['end_time']
-        self.win = match_data['win']
-        self.status = match_data['status']
-        self.counterpart_name = match_data['counterpart_name']
-        self.counterpart_address = match_data['counterpart_address']
-        self.bid_value = match_data['bid_value']
-        self.bid_price = match_data['bid_price']
-        self.win_value = match_data['win_value']
-        self.win_price = match_data['win_price']
-        self.achievement = match_data['achievement']
-        self.settlement = match_data['settlement']
-        self.transaction_hash = match_data['transaction_hash']
-        self.upload = match_data['upload']
-        self.tenders_id = match_data['tenders_id']
+        self.bid_type = match_data["bid_type"]
+        self.start_time = match_data["start_time"]
+        self.end_time = match_data["end_time"]
+        self.win = match_data["win"]
+        self.status = match_data["status"]
+        self.counterpart_name = match_data["counterpart_name"]
+        self.counterpart_address = match_data["counterpart_address"]
+        self.bid_value = match_data["bid_value"]
+        self.bid_price = match_data["bid_price"]
+        self.win_value = match_data["win_value"]
+        self.win_price = match_data["win_price"]
+        self.achievement = match_data["achievement"]
+        self.settlement = match_data["settlement"]
+        self.transaction_hash = match_data["transaction_hash"]
+        self.upload = match_data["upload"]
+        self.tenders_id = match_data["tenders_id"]
+
     # pylint: enable=R0914,C0301
 
 
 class BidSubmit(db.Model, ETBaseMixin):
-    __tablename__ = 'bidsubmit'
+    __tablename__ = "bidsubmit"
     uuid = db.Column(db.String(40), primary_key=True, unique=True, nullable=False)
     bid_type = db.Column(db.String(40))  # sell or buy
     start_time = db.Column(db.DateTime, unique=False, nullable=False)
     end_time = db.Column(db.DateTime, unique=False, nullable=False)
     value = db.Column(db.Float)
     price = db.Column(db.Float)
-    upload_time = db.Column(db.Date, unique=False, nullable=False)
+    upload_time = db.Column(db.DateTime, unique=False, nullable=False)
     # ForeignKey to Bid
-    tenders_id = db.Column(db.String(80), db.ForeignKey('tenders.uuid'), nullable=False)
-    tenders = db.relationship('Tenders')
+    tenders_id = db.Column(db.String(80), db.ForeignKey("tenders.uuid"), nullable=False)
+    tenders = db.relationship("Tenders")
 
     def __init__(self, bid_data):
         self.uuid = str(uuid.uuid4())
-        self.bid_type = bid_data['bid_type']
-        self.start_time = bid_data['start_time']
-        self.end_time = bid_data['end_time']
-        self.value = bid_data['value']
-        self.price = bid_data['price']
+        self.bid_type = bid_data["bid_type"]
+        self.start_time = bid_data["start_time"]
+        self.end_time = bid_data["end_time"]
+        self.value = bid_data["value"]
+        self.price = bid_data["price"]
         self.upload_time = datetime.today()
-        self.tenders_id = bid_data['tenders_id']
+        self.tenders_id = bid_data["tenders_id"]
+
+
+def add_bidsubmit(bid_data):
+    if bid_data:
+        return True
+    return False
+
+
+def edit_bidsubmit(bid_data, user_id):
+    target = BidSubmit.query.filter_by(uuid=bid_data["id"]).first()
+    target.bid_type = bid_data["bid_type"]
+    target.start_time = bid_data["start_time"]
+    target.end_time = bid_data["end_time"]
+    target.value = bid_data["value"]
+    target.price = bid_data["price"]
+    target.upload_time = datetime.today()
+    tender_data = {
+        "bid_type": bid_data["bid_type"],
+        "start_time": bid_data["start_time"],
+        "end_time": bid_data["end_time"],
+        "user_id": user_id,
+    }
+    target.tenders_id = get_tender_id(tender_data)
+    BidSubmit.update(target)
+    return True
+
+
+def get_tender(tender_data):
+    tender = Tenders.query.filter_by(
+        start_time=tender_data["start_time"],
+        end_time=tender_data["end_time"],
+        user_id=tender_data["user_id"],
+    ).first()
+    return tender
+
+
+def get_tender_id(tender_data):
+    tender = get_tender(tender_data)
+    if tender:
+        return tender.uuid
+    Tenders.add(Tenders(tender_data))
+    return get_tender(tender_data).uuid
--- pt/endpoints/bid/resource.py
+++ pt/endpoints/bid/resource.py
@@ -1,9 +1,10 @@
-from flask import jsonify, request
-from flask_restful import Resource
+from datetime import datetime
+from flask import jsonify, make_response
+from flask_restful import Resource, reqparse
 
 from utils.logging import logging
 from utils.oauth import auth, g
-from .model import Tenders, MatchResult, BidSubmit
+from .model import Tenders, MatchResult, BidSubmit, add_bidsubmit, edit_bidsubmit
 
 
 class MatchResultsResource(Resource):
@@ -18,8 +19,8 @@ class MatchResultsResource(Resource):
             bids = [
                 {
                     "id": message.uuid,
-                    "start_time": message.start_time.strftime("%Y/%m/%d %H:%M"),
-                    "end_time": message.end_time.strftime("%Y/%m/%d %H:%M"),
+                    "start_time": message.start_time.strftime("%Y/%m/%d %H"),
+                    "end_time": message.end_time.strftime("%Y/%m/%d %H"),
                     "bid_type": message.bid_type,
                     "win": message.win,
                     "status": message.status,
@@ -54,6 +55,94 @@ class MatchResultsResource(Resource):
 
 
 class BidSubmitResource(Resource):
+    def __init__(self):
+        # common parser for post and put method
+        self._set_common_parser()
+        # add bidsubmit
+        self._set_post_parser()
+        # edit bidsubmit
+        self._set_put_parser()
+        # delete bidsubmit
+        self._set_delete_parser()
+
+    def _set_common_parser(self):
+        self.common_parser = reqparse.RequestParser()
+        self.common_parser.add_argument(
+            "bid_type",
+            type=str,
+            required=True,
+            location="json",
+            help="bid_type is required",
+        )
+        self.common_parser.add_argument(
+            "start_time",
+            type=lambda x: datetime.strptime(x, "%Y/%m/%d %H"),
+            required=True,
+            location="json",
+            help="start_time is required",
+        )
+        self.common_parser.add_argument(
+            "end_time",
+            type=lambda x: datetime.strptime(x, "%Y/%m/%d %H"),
+            required=True,
+            location="json",
+            help="end_time is required",
+        )
+        self.common_parser.add_argument(
+            "value",
+            type=float,
+            required=True,
+            location="json",
+            help="value is required",
+        )
+        self.common_parser.add_argument(
+            "price",
+            type=float,
+            required=True,
+            location="json",
+            help="price is required",
+        )
+
+    def _set_post_parser(self):
+        self.post_parser = self.common_parser.copy()
+
+    def _set_put_parser(self):
+        self.put_parser = self.common_parser.copy()
+        self.common_parser.add_argument(
+            "id",
+            type=str,
+            required=True,
+            location="json",
+            help="Put bidsubmit: id is required",
+        )
+
+    def _set_delete_parser(self):
+        self.delete_parser = reqparse.RequestParser()
+        self.delete_parser.add_argument(
+            "id",
+            type=str,
+            required=True,
+            location="json",
+            help="Delete bidsubmit: id is required",
+        )
+
+    # pylint: disable=R0201
+    def _get_user_bid_ids(self, uuid):
+        bid_ids = [
+            bid.uuid
+            for bid in BidSubmit.query.filter(
+                BidSubmit.tenders_id.in_(
+                    [
+                        tender.uuid
+                        for tender in Tenders.query.filter_by(user_id=uuid).all()
+                    ]
+                )
+            ).all()
+        ]
+        return bid_ids
+
+    # pylint: enable=R0201
+
     # pylint: disable=R0201
     @auth.login_required
     def get(self):
@@ -61,60 +150,75 @@ class BidSubmitResource(Resource):
             "[Get MatchResults Request]\nUser Account:%s\nUUID:%s\n"
             % (g.account, g.uuid)
         )
-        if g.uuid in [tender.user_id for tender in Tenders.query.all()]:
-            bids = [
-                {
-                    "id": message.uuid,
-                    "bid_type": message.bid_type,
-                    "start_time": message.start_time.strftime("%Y/%m/%d %H:%M"),
-                    "end_time": message.end_time.strftime("%Y/%m/%d %H:%M"),
-                    "value": message.value,
-                    "price": message.price,
-                    "upload_time": message.upload_time,
-                }
-                for message in BidSubmit.query.filter(
-                    BidSubmit.tenders_id.in_(
-                        [
-                            tender.uuid
-                            for tender in Tenders.query.filter_by(user_id=g.uuid).all()
-                        ]
-                    )
+        bids = [
+            {
+                "id": message.uuid,
+                "bid_type": message.bid_type,
+                "start_time": message.start_time.strftime("%Y/%m/%d %H"),
+                "end_time": message.end_time.strftime("%Y/%m/%d %H"),
+                "value": message.value,
+                "price": message.price,
+                "upload_time": message.upload_time,
+            }
+            for message in BidSubmit.query.filter(
+                BidSubmit.tenders_id.in_(
+                    [
+                        tender.uuid
+                        for tender in Tenders.query.filter(
+                            Tenders.user_id == g.uuid,
+                            Tenders.start_time >= datetime.today(),
+                        ).all()
+                    ]
                 )
-                .order_by(BidSubmit.start_time.desc())
-                .all()
-            ]
-        else:
-            bids = []
+            )
+            .order_by(BidSubmit.start_time, BidSubmit.value)
+            .all()
+        ]
         response = jsonify(bids)
         return response
 
     @auth.login_required
     def post(self):
-        # Use marshmello to filt input data from body
+        args = self.post_parser.parse_args()
         # insert into db
-        pass
+        if add_bidsubmit(args):
+            return make_response(jsonify({"message": "Accept"}))
+        return make_response(jsonify({"message": "Reject"}))
 
     @auth.login_required
     def put(self):
-        # Use marshmello to filt input data from body
-        # insert into db
-        pass
+        args = self.put_parser.parse_args()
+        if args['start_time'] >= datetime.today():
+            bid_ids = self._get_user_bid_ids(g.uuid)
+            if args["id"] in bid_ids:
+                # insert into db
+                if edit_bidsubmit(args, g.uuid):
+                    return make_response(jsonify({"message": "Accept"}))
+        return make_response(jsonify({"message": "Reject"}))
 
     @auth.login_required
     def delete(self):
-        target_id = str(request.get_json()["id"])
-        bid_ids = [
-            bid.uuid
-            for bid in BidSubmit.query.filter(
-                BidSubmit.tenders_id.in_(
-                    [
-                        tender.uuid
-                        for tender in Tenders.query.filter_by(user_id=g.uuid).all()
-                    ]
-                )
-            ).all()
-        ]
+        args = self.delete_parser.parse_args()
+        target_id = args["id"]
+        bid_ids = self._get_user_bid_ids(g.uuid)
         if target_id in bid_ids:
-            BidSubmit.delete(BidSubmit.query.filter_by(uuid=target_id).first())
+            target = BidSubmit.query.filter_by(uuid=target_id).first()
+            if target.start_time < datetime.today():
+                return make_response(
+                    (
+                        jsonify(
+                            {
+                                "message": "Reject",
+                                "description": "The bid has been closed",
+                            }
+                        ),
+                        400,
+                    )
+                )
+            BidSubmit.delete(target)
+            return make_response(jsonify({"message": "Accept"}))
+        return make_response(
+            jsonify({"message": "Reject", "description": "Invalid ID"}), 400
+        )
 
     # pylint: enable=R0201
