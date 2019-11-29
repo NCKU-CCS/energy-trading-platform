d803e168847befd372e8b5ca57d424e65632664e
iknowright <changchaishi@gmail.com>
2019-11-29 16:48:04 +0800

    feat(token with timeout): provide Long-Lived and Short-Lived Token for authentication purposes
    1. Added a hard-coded APP_SECRET_KEY for the server application, which helps to generate an unique
    serializer that is to generate short-lived token

--- pt/utils/oauth.py
+++ pt/utils/oauth.py
@@ -1,15 +1,24 @@
 from flask import g, jsonify
 from flask_httpauth import HTTPTokenAuth
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 from endpoints.user.model import User
+
+from config import APP_SECRET_KEY
+
+# pylint: disable=C0103
+serializer = Serializer(APP_SECRET_KEY, expires_in=600)
+# pylint: enable=C0103
 
 # pylint: disable=C0103
 auth = HTTPTokenAuth(scheme='Bearer')
 # pylint: enable=C0103
 @auth.verify_token
 def verify_token(token):
+    long_lived_token = serializer.loads(token.encode('utf-8'))
+
     # get username and uuid from database
-    user = User.query.filter_by(tag=token).first()
+    user = User.query.filter_by(tag=long_lived_token).first()
     if user:
         g.uuid = user.uuid
         g.account = user.account
