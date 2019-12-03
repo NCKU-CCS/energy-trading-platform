e6583df73db0845e6d5fd066fda78c5a03dbbec3
iknowright <changchaishi@gmail.com>
2019-12-03 15:28:40 +0800

    improvement(oauth): try and catch signature exception and improve authentication error handling
    1. Delete unwanted print line in resource.py. 2. Try and catch the exception of short-lived token
    signature. 3. Add flask global context to catch exception message, can be handler bu error handler.

--- pt/utils/oauth.py
+++ pt/utils/oauth.py
@@ -1,6 +1,6 @@
 from flask import g, jsonify
 from flask_httpauth import HTTPTokenAuth
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSignature
 
 from endpoints.user.model import User
 
@@ -13,9 +13,17 @@ serializer = Serializer(APP_SECRET_KEY, expires_in=600)
 # pylint: disable=C0103
 auth = HTTPTokenAuth(scheme='Bearer')
 # pylint: enable=C0103
+
+
 @auth.verify_token
 def verify_token(token):
-    long_lived_token = serializer.loads(token.encode('utf-8'))
+    try:
+        long_lived_token = serializer.loads(token.encode('utf-8'))
+    except BadSignature:
+        g.error_message = BadSignature.message
+        if g.error_message.find('Signature') >= 0 and g.error_message.find('does not match') >= 0:
+            g.error_message = 'Unauthorized access'
+        return False
 
     # get username and uuid from database
     user = User.query.filter_by(tag=long_lived_token).first()
@@ -23,9 +31,10 @@ def verify_token(token):
         g.uuid = user.uuid
         g.account = user.account
         return True
+    g.error_message = 'Access denied'
     return False
 
 
 @auth.error_handler
 def unauthorized():
-    return jsonify({'error': 'Unauthorized access'}), 401
+    return jsonify({'error': g.error_message}), 401
