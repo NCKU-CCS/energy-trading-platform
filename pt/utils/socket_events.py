c6a87ae50014b9b22e1b45a6209054c023159d03
iknowright <changchaishi@gmail.com>
2019-12-23 16:04:40 +0800

    feat(socketio): add socketio to the server

---
+++ pt/utils/socket_events.py
@@ -0,0 +1,54 @@
+from flask import request
+# pylint: disable=W0622
+from flask_socketio import SocketIO, emit, ConnectionRefusedError
+# pylint: enable=W0622
+from itsdangerous import BadSignature
+
+from utils.oauth import serializer
+from utils.logging import logging
+from endpoints.user.model import User
+
+
+# pylint: disable=C0103
+socketio = SocketIO()
+# pylint: enable=C0103
+
+
+@socketio.on('connect')
+def connected():
+    verified = False
+    # Verify token
+    token = request.args.get('token')
+    if token is not None:
+        try:
+            long_lived_token = serializer.loads(token.encode('utf-8'))
+            user = User.query.filter_by(tag=long_lived_token).first()
+            if user:
+                logging.info(
+                    "[SocketIO]\nClient connected"
+                )
+                verified = True
+            else:
+                logging.info(
+                    "[SocketIO]\nLong-lived token does not match any database entry"
+                )
+        # BadSignature catch nothing
+        except BadSignature:
+            logging.info(
+                "[SocketIO]\nShort-lived token expired or invalid"
+            )
+    else:
+        logging.info(
+            "[SocketIO]\nShort-lived token not provided"
+        )
+
+    if verified is False:
+        raise ConnectionRefusedError("Unauthorized access")
+
+
+@socketio.on('greeting')
+def on_my_event(json):
+    logging.info(
+        "[SocketIO]\n%s" % json
+    )
+    emit("notification", "Hi everyone", broadcast=True)
