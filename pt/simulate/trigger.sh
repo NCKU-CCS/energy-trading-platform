bb00de82de8da4e2a0119323287478ce3002840b
lhleung <grandq33769@gmail.com>
2020-06-08 03:13:51 +0800

    fix(trigger.sh): Change log path

--- pt/simulate/trigger.sh
+++ pt/simulate/trigger.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
 cd $BASEDIR/../../
-/usr/local/bin/pipenv run python3 -m pt.simulate.main -b $1 -p $2 >> $HOME/simulator_log/$1.log 2>&1
+/usr/local/bin/pipenv run python3 -m pt.simulate.main -b $1 -p $2 >> /tmp/$1.log 2>&1
