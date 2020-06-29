e64340d6b7a818ed285f3dbe62880b84f144e205
Steven HH Chen <hhchen@netdb.csie.ncku.edu.tw>
2020-06-29 05:13:42 -0700

    Add scripts for different testing terms
    These scripts is mainly for integrating pytest usage
    The authorization key sharing can be improved

---
+++ eptest/req/ep_login.py
@@ -0,0 +1,6 @@
+import requests
+print("Testing endpoint...")
+
+res = requests.post("http://140.116.247.117:5000/login", json={"account":"SGESC_D_BEMS","password":"test"})
+print(res.status_code)
+print(res.json())
---
+++ eptest/req/ep_match_res.py
@@ -0,0 +1,8 @@
+import requests
+
+bearer="Bearer eyJhbGciOiJIUzUxMiIsImlhdCI6MTU5MzI0OTU1NCwiZXhwIjoxNTkzMzM1OTU0fQ.IjZjNzMxZmJhZDY1ZWU3NTEzYmM4NGVkN2FiNmRjNWE5Mjg2MTNlNzdjNjA2OGJiZWVjNWEwMzFlYWVkN2M3YjUi.uAzW9BPsIFH-B-OgeoVs8EpIqvT4SnsF4WOhnuY6kq2Ie6MC-O3ztg43v9T5mS21J3Fax4vl2V5uRhEhHw5Yow"
+req = requests.get("http://140.116.247.117:5000/matchresult", headers={"Content-Type": "application/json", "Authorization": bearer})
+
+
+print(req.status_code)
+print(req.json())
