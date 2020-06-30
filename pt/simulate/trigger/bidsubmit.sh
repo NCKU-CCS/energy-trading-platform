a39eacecd15208e9ab3b62d3fcaf7848a1e26bda
lhleung <grandq33769@gmail.com>
2020-07-01 00:21:10 +0800

    fix(trigger): fix bidsubmit trigger time

--- pt/simulate/trigger/bidsubmit.sh
+++ pt/simulate/trigger/bidsubmit.sh
@@ -6,8 +6,10 @@
 #! -v: bidding value
 #! -m: bidding price
 #! -t: bidding type
+#! UTC+0 -> UTC+8
+#! 1600 -> 0000
 BASEDIR=$(dirname "$0")
-START=$(date +"%Y/%m/%d ")00
-END=$(date +"%Y/%m/%d " --date="+1 days")00
+START=$(date +"%Y/%m/%d ")16
+END=$(date +"%Y/%m/%d " --date="+1 days")17
 cd $BASEDIR/../../../
 /usr/local/bin/pipenv run python3 -m pt.simulate.bidsubmit -a $1 -p $2 -s "$START" -e "$END" -v $3 -m $4 -t $5 >> /tmp/bidsubmit-$1.log 2>&1
