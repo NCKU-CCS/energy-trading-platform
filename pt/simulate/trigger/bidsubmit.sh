0814d6d0492e5f75ad46ab806170977d84870c35
lhleung <grandq33769@gmail.com>
2020-06-30 23:19:48 +0800

    fix(trigger): Update the directory

--- pt/simulate/trigger/bidsubmit.sh
+++ pt/simulate/trigger/bidsubmit.sh
@@ -9,5 +9,5 @@
 BASEDIR=$(dirname "$0")
 START=$(date +"%Y/%m/%d ")00
 END=$(date +"%Y/%m/%d " --date="+1 days")00
-cd $BASEDIR/../../
+cd $BASEDIR/../../../
 /usr/local/bin/pipenv run python3 -m pt.simulate.data -a $1 -p $2 -s $3 -e $4 -v $5 -m $6 -t $7 >> /tmp/bidsubmit-$1.log 2>&1
