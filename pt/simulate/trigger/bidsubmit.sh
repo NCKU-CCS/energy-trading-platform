ddc979f8ff2496dc34ea82bde5efb0693ee3bed5
lhleung <grandq33769@gmail.com>
2020-06-30 23:30:23 +0800

    fix(trigger): Fix bidsubmit.sh args

--- pt/simulate/trigger/bidsubmit.sh
+++ pt/simulate/trigger/bidsubmit.sh
@@ -10,4 +10,4 @@ BASEDIR=$(dirname "$0")
 START=$(date +"%Y/%m/%d ")00
 END=$(date +"%Y/%m/%d " --date="+1 days")00
 cd $BASEDIR/../../../
-/usr/local/bin/pipenv run python3 -m pt.simulate.bidsubmit -a $1 -p $2 -s $START -e $END -v $3 -m $4 -t $5 >> /tmp/bidsubmit-$1.log 2>&1
+/usr/local/bin/pipenv run python3 -m pt.simulate.bidsubmit -a $1 -p $2 -s "$START" -e "$END" -v $3 -m $4 -t $5 >> /tmp/bidsubmit-$1.log 2>&1
