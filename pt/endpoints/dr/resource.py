e65a7e76c657fbad495380b2df5a9a531b8d0b04
tizan <tizan0826@gmail.com>
2021-02-20 16:28:21 +0800

    feat: get dr bids according to different roles

--- pt/endpoints/dr/resource.py
+++ pt/endpoints/dr/resource.py
@@ -2,11 +2,13 @@ import uuid
 from datetime import datetime, timedelta
 
 from flask_restful import Resource, reqparse
+from sqlalchemy import or_, and_
 from loguru import logger
 
 from utils.oauth import auth, g
 
 from .model import DRBidModel, aggregator_accept, user_add_bid, get_user_by_account
+from ..user.model import User
 
 
 class DRBid(Resource):
@@ -59,11 +61,20 @@ class DRBid(Resource):
     def get(self):
         logger.info(f"[Get DRBid Request]\nUser Account:{g.account}\nUUID:{g.uuid}\n")
         args = self.get_parser.parse_args()
-        criteria = [DRBidModel.start_time >= args["date"], DRBidModel.start_time < args["date"] + timedelta(days=1)]
-        if g.role != "aggregator":
-            # user can only get their own bids
-            criteria.append(DRBidModel.executor == g.account)
-        dr_bids = DRBidModel.query.filter(*criteria).order_by(DRBidModel.start_time).all()
+
+        date = [DRBidModel.start_time >= args["date"], DRBidModel.start_time < args["date"] + timedelta(days=1)]
+        accounts = []
+        if g.role == "user":
+            date.append(DRBidModel.executor == g.account)
+        elif g.role == "aggregator":
+            accounts = User.query.filter(User.role.in_(["aggregator", "user"])).all()
+        elif g.role == "tpc":
+            accounts = User.query.filter_by(role="aggregator").all()
+        roles = [DRBidModel.executor == user.account for user in accounts]
+
+        dr_filter = {and_(*date, or_(*roles))}
+        dr_bids = DRBidModel.query.filter(*dr_filter).order_by(DRBidModel.start_time).all()
+
         return [
             {"uuid": bid.uuid, "executor": bid.executor, "volume": bid.volume, "price": bid.price} for bid in dr_bids
         ]
